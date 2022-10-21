e703de8a432eb66e6c770189cd56c5fe8ddda9bf
jorgeAgoiz <jorgeagoiz@gmail.com>
2022-10-21 22:49:28 +0200

    Feat: CORS Enabled

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   const config = new DocumentBuilder()
     .setTitle('Formula One Data And Statistics')
     .setDescription('REST API about formula one data and statistics')
