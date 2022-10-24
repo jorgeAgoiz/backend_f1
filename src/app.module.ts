8714432245db63b16f7b20e497f92445707d5dca
jorgeAgoiz <jorgeagoiz@gmail.com>
2022-10-24 22:34:16 +0200

    new config database

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,21 +5,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { DriversModule } from './drivers/drivers.module';
 import { CircuitsModule } from './circuits/circuits.module';
 import { TeamsModule } from './teams/teams.module';
-import { Circuits } from './circuits/circuits.entity';
-import { Drivers } from './drivers/drivers.entity';
-import { Teams } from './teams/teams.entity';
+import { DatabaseConfiguration } from './config/database-config';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'root',
-      database: 'formula_one',
-      entities: [Circuits, Drivers, Teams],
-      synchronize: false,
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      useClass: DatabaseConfiguration,
     }),
     DriversModule,
     CircuitsModule,
