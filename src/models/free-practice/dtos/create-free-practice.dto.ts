5945c30e97b460d34a0d31f27239eab367c67fa5
jorgeAgoiz <jorgeagoiz@gmail.com>
2022-10-27 21:17:41 +0200

    New DTO created

---
+++ src/models/free-practice/dtos/create-free-practice.dto.ts
@@ -0,0 +1,28 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNumber, IsString } from 'class-validator';
+
+export class CreateFreePracticeDto {
+  @IsNumber()
+  @ApiProperty()
+  grand_prix: number;
+
+  @IsNumber()
+  @ApiProperty()
+  fp_number: number;
+
+  @IsNumber()
+  @ApiProperty()
+  position: number;
+
+  @IsNumber()
+  @ApiProperty()
+  laps: number;
+
+  @IsString()
+  @ApiProperty()
+  fast_lap: string;
+
+  @IsString()
+  @ApiProperty()
+  average_speed: number;
+}
