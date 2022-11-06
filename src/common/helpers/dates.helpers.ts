5cf3fdac59b2b29c534561bba596975f3eb51539
jorgeAgoiz <jorgeagoiz@gmail.com>
2022-11-06 19:11:12 +0100

    function to parse time to miliseconds modified

--- src/common/helpers/dates.helpers.ts
+++ src/common/helpers/dates.helpers.ts
@@ -1,10 +1,25 @@
-export const fastLapStringToMiliseconds = (value: string): number => {
+interface Args {
+  value: string;
+  type: string;
+}
+
+export const timeStringToMiliseconds = ({ value, type }: Args): number => {
   const firstSplit: Array<string> = value.split("'");
   const secondSplit: Array<string> = firstSplit[1].split('.');
 
-  const minutes: number = parseInt(firstSplit[0]) * 60 * 1000;
-  const seconds: number = parseInt(secondSplit[0]) * 1000;
-  const miliseconds: number = parseInt(secondSplit[1]);
+  if (type === 'total') {
+    const hours: number =
+      parseInt(firstSplit[0].split(':')[0]) * 60 * 60 * 1000;
+    const minutes: number = parseInt(firstSplit[0].split(':')[1]) * 60 * 1000;
+    const seconds: number = parseInt(secondSplit[0]) * 1000;
+    const miliseconds: number = parseInt(secondSplit[1]);
 
-  return minutes + seconds + miliseconds;
+    return hours + minutes + seconds + miliseconds;
+  } else {
+    const minutes: number = parseInt(firstSplit[0]) * 60 * 1000;
+    const seconds: number = parseInt(secondSplit[0]) * 1000;
+    const miliseconds: number = parseInt(secondSplit[1]);
+
+    return minutes + seconds + miliseconds;
+  }
 };
