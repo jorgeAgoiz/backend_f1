46edc0937eda8c2f5f25f2cf5ec72d6f26bc85b7
jorgeAgoiz <jorgeagoiz@gmail.com>
2022-11-16 20:37:16 +0100

    all unit testing finished

--- src/models/grand-prix/grand-prix.controller.spec.ts
+++ src/models/grand-prix/grand-prix.controller.spec.ts
@@ -1,13 +1,20 @@
-/* import { Test, TestingModule } from '@nestjs/testing';
+import { Test, TestingModule } from '@nestjs/testing';
 import { GrandPrixController } from './grand-prix.controller';
+import { GrandPrixService } from './grand-prix.service';
 
 describe('GrandPrixController', () => {
   let controller: GrandPrixController;
 
+  const mockGrandPrixService = {};
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [GrandPrixController],
-    }).compile();
+      providers: [GrandPrixService],
+    })
+      .overrideProvider(GrandPrixService)
+      .useValue(mockGrandPrixService)
+      .compile();
 
     controller = module.get<GrandPrixController>(GrandPrixController);
   });
@@ -15,4 +22,4 @@ describe('GrandPrixController', () => {
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
-}); */
+});
--- src/models/grand-prix/grand-prix.service.spec.ts
+++ src/models/grand-prix/grand-prix.service.spec.ts
@@ -1,12 +1,22 @@
-/* import { Test, TestingModule } from '@nestjs/testing';
+import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { GrandPrix } from './grand-prix.entity';
 import { GrandPrixService } from './grand-prix.service';
 
 describe('GrandPrixService', () => {
   let service: GrandPrixService;
 
+  const mockGrandPrixRepository = {};
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [GrandPrixService],
+      providers: [
+        GrandPrixService,
+        {
+          provide: getRepositoryToken(GrandPrix),
+          useValue: mockGrandPrixRepository,
+        },
+      ],
     }).compile();
 
     service = module.get<GrandPrixService>(GrandPrixService);
@@ -15,4 +25,4 @@ describe('GrandPrixService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
-}); */
+});
