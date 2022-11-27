03a430e14d2744efc673d9a04636a1841d76ffb4
jorgeAgoiz <jorgeagoiz@gmail.com>
2022-11-27 20:57:15 +0100

    Small test

--- test/drivers.e2e-spec.ts
+++ test/drivers.e2e-spec.ts
@@ -23,7 +23,7 @@ describe('Drivers (e2e)', (): void => {
 
     expect(response.status).toEqual(200);
     expect(Array.isArray(response.body)).toBeTruthy();
-    expect(response.body).toHaveLength(33);
+    expect(response.body).toHaveLength(333);
   });
 
   it('/drivers/:id (GET) OK', async (): Promise<void> => {
